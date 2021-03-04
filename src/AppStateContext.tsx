fdb243726fceba19955ca1b00bcd7d29e437381e
samTer <samuelbryo@gmail.com>
2021-03-03 19:23:17 -0800

    feat: add context logic

---
+++ src/AppStateContext.tsx
@@ -0,0 +1,45 @@
+import React, { createContext, useContext } from 'react';
+interface Task {
+  id: string;
+  text: string;
+}
+interface List {
+  id: string;
+  text: string;
+  tasks: Task[];
+}
+export interface AppState {
+  lists: List[];
+}
+
+interface AppStateContextProps {
+  state: AppState;
+}
+
+const appData: AppState = {
+  lists: [
+    {
+      id: '0',
+      text: 'To Do',
+      tasks: [{ id: 'c0', text: 'Generate app scaffold' }],
+    },
+    {
+      id: '1',
+      text: 'In Progress',
+      tasks: [{ id: 'c2', text: 'Learn Typescript' }],
+    },
+    {
+      id: '2',
+      text: 'Done',
+      tasks: [{ id: 'c3', text: 'Begin to use static typing' }],
+    },
+  ],
+};
+
+const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps);
+export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
+  return <AppStateContext.Provider value={{ state: appData }}>{children}</AppStateContext.Provider>;
+};
+export const useAppState = () => {
+  return useContext(AppStateContext);
+};
